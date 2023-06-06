caf42a272599ac74e1e1eb00d3af5756246689e6
Sergio <sebermudezg@unal.edu.co>
2023-06-06 17:26:53 -0500

    This is the final

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,4 +38,5 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT COUNT(*) AS CANT, strftime('%Y', c14) AS YEAR FROM tbl1 WHERE YEAR = '2018' GROUP BY YEAR
+SELECT COUNT(*) FROM tbl1
+WHERE strftime('%Y', c14) = '2018'
